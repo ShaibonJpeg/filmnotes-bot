7d1ac487996b1efe079c302426913a37abc9f1ca
ShaibonJpeg <shaibonin@gmail.com>
2020-05-31 22:33:00 +0300

    edit

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const TelegramBot = require('node-telegram-bot-api')
 const TOKEN = '1202168944:AAG_B7qkSz2b9rj4Ii97uhzI646cU0Qk0qY'
 const bot = new TelegramBot(TOKEN, {
-  polling: {
-    interval: 300,
-    autoStart: true,
-    params: {
-      timeout: 10
-    }
-  }
+  polling: true
+  // polling: {
+  //   interval: 300,
+  //   autoStart: true,
+  //   params: {
+  //     timeout: 10
+  //   }
+  // }
 });
 var films = [];
 bot.onText(/\/addfilm (.+)/, (msg,match) => {
