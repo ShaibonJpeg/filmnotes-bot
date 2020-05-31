48bba4b98e1a5a290d177a0b725fb9a3a9f5819a
ShaibonJpeg <shaibonin@gmail.com>
2020-05-31 18:23:52 +0300

    edit

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const bot = new TelegramBot(TOKEN, {polling: true})
 
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://shaibonjpeg:<password>@cluster0-yn2yo.mongodb.net/test?retryWrites=true&w=majority";
+const uri = "mongodb+srv://shaibonjpeg:Shaibon2019@cluster0-yn2yo.mongodb.net/test?retryWrites=true&w=majority";
 
 
 
 bot.onText(/\/db (.+)/, (msg, match) => {
 
   MongoClient.connect(uri, function(err,client) {
-    const db = client.db('filmbot');
+    const db = client.db("filmbot");
     // var film = {firstName : `${msg.from.first_name}`, film_name: `${match[1]}`, chat_id: `${msg.chat.id}`};
     var film = {firstName : "Инсаф"};
-    const collection = db.collection('films');
+    const collection = db.collection("films");
     collection.insertOne({"firstName" : "Инсаф"}, function(err,result){
 
 
