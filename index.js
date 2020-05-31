039e23f6bcf13c5ffbdac6d2984d3bcf17c65046
ShaibonJpeg <shaibonin@gmail.com>
2020-05-31 16:43:26 +0300

    fix '==>' to '=>'

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const TOKEN = '1202168944:AAG_B7qkSz2b9rj4Ii97uhzI646cU0Qk0qY'
 
 const bot = new TelegramBot(TOKEN, {polling: true})
 
-bot.on('message', msg ==> {
+bot.on('message', msg => {
   bot.sendMessage(msg.chat.id, `Hello ${msg.from.first_name}`)
 })
